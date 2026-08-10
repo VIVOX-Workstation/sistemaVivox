68554e538218e28abdd3671b99276f46df6d774e
Kelson Cosme <kelson.almeida123@gmail.com>
2026-08-10 11:14:30 -0400

    Fix host binding para Docker

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
